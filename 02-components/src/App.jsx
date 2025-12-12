94a58a7e1d90459c94efde039919075d79fcb744
PiyushBytes <dawnpiyush384@gmail.com>
2025-12-13 04:10:48 +0530

    Enhance App component with updated content and integrate card component; improve styling in index.css

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import card from "./components/card";
 
 const App = () => {
   return (
     <div>
       <div className = "card">
         <h1>Piyush Dawn</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quisquam?</p>
+        <p>A passionate learner exploring the world of web development, UI design, and modern JavaScript frameworks.</p>
       </div>
+      {card()}
+
+
     </div>
+
   )
 }
 
